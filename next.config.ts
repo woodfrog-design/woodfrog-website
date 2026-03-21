d34e84b4e9b17318cffba7a57271c3738dd0a237
VANSHTalyani <vanshtalyani12@gmail.com>
2026-03-22 02:06:19 +0530

    redirection to data-agents

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/services/ai-ml-strategy",
-        destination: "/ai-agents",
+        destination: "/data-agents",
         permanent: true,
       },
       {
